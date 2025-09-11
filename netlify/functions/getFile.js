dcc2e38d013fc377d13ce8806001da25d625d207
Kazuma1107 <95493229+Kazuma1107@users.noreply.github.com>
2025-09-11 17:34:24 +0900

    Update getFile.js

--- netlify/functions/getFile.js
+++ netlify/functions/getFile.js
@@ -5,13 +5,17 @@ exports.handler = async function(event, context) {
     const { fileKey } = event.queryStringParameters;
     if (!fileKey) return { statusCode: 400, body: "fileKey is required" };
 
-    const resp = await fetch(`https://${process.env.KINTONE_SUBDOMAIN}.cybozu.com/k/v1/file.json?fileKey=${fileKey}`, {
-      headers: {
-        "X-Cybozu-API-Token": process.env.KINTONE_IMAGE_API_TOKEN
+    const resp = await fetch(
+      `https://${process.env.KINTONE_SUBDOMAIN}.cybozu.com/k/v1/file.json?fileKey=${fileKey}`,
+      {
+        headers: {
+          "X-Cybozu-API-Token": process.env.KINTONE_IMAGE_API_TOKEN
+        }
       }
-    });
+    );
 
-    const buffer = await resp.arrayBuffer();       // バイナリとして取得
+    // バイナリとして取得して Base64 に変換
+    const buffer = await resp.arrayBuffer();
     const base64 = Buffer.from(buffer).toString("base64");
 
     return {
